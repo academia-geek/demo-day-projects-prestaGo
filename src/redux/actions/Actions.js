6084dd980880368c2ef7c5a245bd40dabe27bfd2
Angie Gordillo <scarletrockbell@gmail.com>
2022-06-02 23:27:33 -0500

    Configuracion de login

--- src/redux/actions/Actions.js
+++ src/redux/actions/Actions.js
@@ -1,4 +1,19 @@
 import { LoginReducer } from "../Reducers/Reducers"
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
+
+export const loginAsync = (email, password)=>{
+    return(dispatch)=>{
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth,email, password)
+        .then(({user})=>{
+            dispatch(loginSync(user.email, user.password))
+        })
+        .catch (error =>{
+            alert('Incorrecto')
+        })
+    }
+}
+
 
 export const loginSync = (email, password)=>{
     return{
